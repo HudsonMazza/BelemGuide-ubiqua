2af89f9357b29b4e4dd673a4512ddc54f3a32167
HudsonMazza <mazzahudson05@outlook.com>
2024-04-26 19:20:05 -0300

    belem-guide está completa para semana ubíqua

---
+++ README.md
@@ -0,0 +1,3 @@
+O sistema pode ser rodado simplemente digitando "npm start" no seu prompt de comando    -  Você precisa ter o NODE.js instalado
+
+ou entrando em  https://belem-guide-7ef0be91ed89.herokuapp.com/
--- app.js
+++ app.js
@@ -1,45 +1,85 @@
-let nextBtn = document.querySelector('.next')
-let prevBtn = document.querySelector('.prev')
+const express = require('express');
+const path = require('path');
+const rout= require("./routes/routes.js")
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
 
-let slider = document.querySelector('.slider')
-let sliderList = slider.querySelector('.slider .list')
-let thumbnail = document.querySelector('.slider .thumbnail')
-let thumbnailItems = thumbnail.querySelectorAll('.item')
-
-thumbnail.appendChild(thumbnailItems[0])
-
-// Function for next button 
-nextBtn.onclick = function() {
-    moveSlider('next')
-}
-
-
-// Function for prev button 
-prevBtn.onclick = function() {
-    moveSlider('prev')
-}
+/// CONFIG
+const app = express();
+app.use('/assets', express.static(__dirname +'/assets' ))
+app.use("/css", express.static(path.join(__dirname, './css/')));
+app.use('/js', express.static(path.join(__dirname, './js/')))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.set('view engine', 'hbs');
+const publicDir = path.join(__dirname, './public');
+app.use(express.static(publicDir));
+///
 
 
-function moveSlider(direction) {
-    let sliderItems = sliderList.querySelectorAll('.item')
-    let thumbnailItems = document.querySelectorAll('.thumbnail .item')
-    
-    if(direction === 'next'){
-        sliderList.appendChild(sliderItems[0])
-        thumbnail.appendChild(thumbnailItems[0])
-        slider.classList.add('next')
-    } else {
-        sliderList.prepend(sliderItems[sliderItems.length - 1])
-        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
-        slider.classList.add('prev')
-    }
+const LocalStrategy = require('passport-local').Strategy;
 
 
-    slider.addEventListener('animationend', function() {
-        if(direction === 'next'){
-            slider.classList.remove('next')
-        } else {
-            slider.classList.remove('prev')
+
+
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    db.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
+        if (error) {
+            return done(error);
         }
-    }, {once: true}) // Remove the event listener after it's triggered once
-}
+
+        if (results.length === 0) {
+            return done(null, false, { message: 'Usuário não encontrado' });
+        }
+
+        const user = results[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return done(null, false, { message: 'Credenciais inválidas' });
+        }
+
+        return done(null, user);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); // Serializa o usuário
+});
+
+passport.deserializeUser((id, done) => {
+    db.query('SELECT * FROM users WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            return done(error);
+        }
+
+        const user = results[0];
+        done(null, user); // Desserializa o usuário
+    });
+});
+// Configuração das sessões e flash messages
+app.use(session({
+    secret: 'nodeusado',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize()); // Inicializa o passport
+app.use(passport.session()); // Usa sessões do passport
+app.use(flash());
+
+
+
+
+
+
+app.use('/', rout);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
+module.exports = app;
